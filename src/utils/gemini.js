a063c005488316dfa7941f9f8861784e1d2dd7d3
sriharshitha-konkathi <sriharshithakonkathi@gmail.com>
2026-05-23 17:51:22 +0530

    removed gemini api file

--- src/utils/gemini.js
+++
@@ -1,8 +0,0 @@
-import { GoogleGenerativeAI } from "@google/generative-ai";
-
-const genAI = new GoogleGenerativeAI(process.env.REACT_APP_GEMINI_KEY);
-
-export const model = genAI.getGenerativeModel(
-  { model: "gemini-2.0-flash" },
-  { apiVersion: "v1" },
-);
